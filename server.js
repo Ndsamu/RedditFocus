30b12a4d34e75c0600acb54839eb0e25446d2cf6
Nate Samuelson <samue289@umn.edu>
2019-02-21 12:20:46 -0600

    Testing app

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const middleware = [
+    express.static(path.join(__dirname, 'public')),
+    bodyParser.urlencoded({ extended: true }),
+    bodyParser.json()
+]
+
+app.use(middleware)
+
+app.use('/', routes)
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!")
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
