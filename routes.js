9dae745f592cb353f02f22de4394b2714a33a66a
Nate Samuelson <samue289@umn.edu>
2019-02-22 09:15:10 -0600

    Unsuccessful post body

--- routes.js
+++ routes.js
@@ -11,13 +11,26 @@ const Snoostorm = require('snoostorm')
 
 const redditConfig = new Snoowrap({
     userAgent: 'user agent web:com.redditfocus:v0.0 (by /u/ndsamu)',
+    //refreshToken: process.env.REFRESH_TOKEN,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
     password: process.env.REDDIT_PASS
 })
 
-const reddit = new Snoostorm(redditConfig)
+//const reddit = new Snoostorm(redditConfig)
+
+redditConfig.getSubmission('3g8u2t').title.then(onSuccess)
+
+function onSuccess(result) {
+    console.log(result)
+}
+
+/*
+post.on('post'), (post) => {
+    console.log(post)
+}
+
 
 const streamOpts = {
     subreddit: 'all',
@@ -30,6 +43,7 @@ comments.on('comment', (comment) => {
     console.log(comment.author)
     console.log(comment.body)
 })
+*/
 
 router.get('/', (req,res) => {
     res.send("Website UP")
